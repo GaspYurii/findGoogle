b5d1e133233a11386e8e6aa5f74c7e84c4019a30
Yurii <67853263+GaspYurii@users.noreply.github.com>
2021-07-25 20:00:25 +0300

    Fix of View

--- src/main/java/com/melon/parserquery/view/ConsoleView.java
+++ src/main/java/com/melon/parserquery/view/ConsoleView.java
@@ -17,24 +17,52 @@ public class ConsoleView {
     private Locale locale = Locale.UK;
     private ResourceBundle rb = ResourceBundle.getBundle("lang", locale);
 
+    private String getInputFromKeyboard() {
+        return scanner.nextLine();
+    }
+
+    private String format(SearchQueryDTO model, Locale locale) {
+        return String.format(rb.getString(ViewConstants.RESULTS),
+                NumberFormat.getInstance(locale).format(model.getResultCount()),
+                model.getSearcher());
+    }
+
+    /**
+     * Print string to console
+     *
+     * @param s String to be output to the console
+     */
     public void println(String s) {
         System.out.println(s);
     }
 
+    /**
+     * Construct a string from SearchQueryDTO model parameters
+     *
+     * @param model  SearchQueryDTO
+     * @param locale localization for formatting constructed string
+     */
     public void printSearchQueryDTO(SearchQueryDTO model, Locale locale) {
         println(format(model, locale));
     }
 
+    /**
+     * Print to console every model from collection of SearchQueryDTO
+     *
+     * @param models collection of SearchQueryDTO models
+     * @param locale localization for formatting constructed string
+     */
     public void printSearchQueryDTO(Collection<SearchQueryDTO> models, Locale locale) {
         for (SearchQueryDTO model : models) {
             printSearchQueryDTO(model, locale);
         }
     }
 
-    private String getInputFromKeyboard() {
-        return scanner.nextLine();
-    }
-
+    /**
+     * Receives input from keyboard without leading and trailing spaces. This method validate not empty input
+     *
+     * @return input as String
+     */
     public String getInput() {
         String input = getInputFromKeyboard().trim();
 
@@ -46,6 +74,11 @@ public class ConsoleView {
         return input;
     }
 
+    /**
+     * Receives input from keyboard and get
+     *
+     * @return List of Searcher objects
+     */
     public List<Searcher> getSearchers() {
         Searcher[] searchers = null;
         logger.debug("{}", rb.getLocale());
@@ -60,24 +93,32 @@ public class ConsoleView {
         return Arrays.asList(searchers);
     }
 
-    private String format(SearchQueryDTO model, Locale locale) {
-        return String.format(rb.getString(ViewConstants.RESULTS),
-                NumberFormat.getInstance(locale).format(model.getResultCount()),
-                model.getSearcher());
-    }
-
+    /**
+     * Receives input from keyboard to save searchers or not
+     *
+     * @param searchers need to log saved searchers
+     * @return "true" if searchers are saved
+     */
     public boolean isSavedChoiceParser(List<Searcher> searchers) {
         String input;
+        String yes = rb.getString(ViewConstants.YES);
+        String y = yes.substring(0, 1);
+        String no = rb.getString(ViewConstants.NO);
+        String n = no.substring(0, 1);
+        boolean result;
         do {
-            println(rb.getString(ViewConstants.SAVE_PARSERS)
-                    + " " + rb.getString(ViewConstants.ENTER) + " "
-                    + rb.getString(ViewConstants.YES)
-                    + "/"
-                    + rb.getString(ViewConstants.NO)
+            println(String.format("%s %s %s(%s)/%s(%s)",
+                    rb.getString(ViewConstants.SAVE_PARSERS),
+                    rb.getString(ViewConstants.ENTER),
+                    yes,
+                    y,
+                    no,
+                    n)
             );
             input = getInput();
-        } while (rb.getString(ViewConstants.YES).equals(input) == rb.getString(ViewConstants.NO).equals(input));
-        boolean result = rb.getString(ViewConstants.YES).equals(input);
+            result = yes.equalsIgnoreCase(input) || y.equalsIgnoreCase(input.substring(0, 1));
+        } while (result == (no.equalsIgnoreCase(input) || n.equalsIgnoreCase(input)));
+
         if (result) {
             logger.info("Search engine selection saved: {}", searchers);
         }
@@ -87,7 +128,7 @@ public class ConsoleView {
     /**
      * Print menu to console and get locale from menu item input
      *
-     * @return object of Locale class or null
+     * @return object of Locale class
      */
     public Locale getLocaleFromInput() {
         Locale locale1;
